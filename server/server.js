e9872a85e48dfe1b5f8d633a23874d460706d2c0
iamSt3el <himanshudhaka9166@gmail.com>
2025-10-22 19:05:59 +0530

    fixed bugs

--- server/server.js
+++ server/server.js
@@ -7,25 +7,20 @@ dotenv.config();
 
 const app = express();
 
-// Connect to MongoDB
 connectDB();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Routes
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/projects', require('./routes/projectRoutes'));
 app.use('/api/files', require('./routes/fileRoutes'));
 
-// Health check route
 app.get('/api/health', (req, res) => {
   res.json({ status: 'ok', message: 'Server is running' });
 });
 
-// Error handling middleware
 app.use((err, req, res, next) => {
   res.status(err.status || 500).json({
     success: false,
@@ -37,4 +32,5 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
